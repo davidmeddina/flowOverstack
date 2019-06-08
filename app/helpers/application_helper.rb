13670e2ef97b445e9b56239a8effa6f564961319
david medina <davidmeddina@gmail.com>
2019-06-08 16:15:08 -0500

    flash messages done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def flash_msg(key)
+    case key
+      when 'notice' then 'alert alert-dismissible alert-info'
+      when 'success' then 'alert alert-dismissible alert-success'
+      when 'error' then 'alert alert-dismissible alert-danger'
+      when 'alert' then 'alert alert-dismissible alert-warning'
+    end
+  end
 end
