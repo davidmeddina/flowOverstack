2a7e79e98d7f54ce6e8a7469ff29d5be1dc0bd57
david medina <davidmeddina@gmail.com>
2019-06-20 15:49:52 -0500

    Vote model created

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,7 +1,22 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: answers
+#
+#  id          :integer          not null, primary key
+#  question_id :integer
+#  user_id     :integer
+#  content     :text
+#  rating      :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+
 class Answer < ApplicationRecord
   belongs_to :question
   belongs_to :user
   has_many :comments, as: :commentable
+  has_many :votes, as: :votable
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,19 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: questions
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  title      :string
+#  content    :text
+#  rating     :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+
 class Question < ApplicationRecord
   belongs_to :user
   has_many :answers, dependent: :destroy
